0d06013b5a6951fde89b61c7e3df2db607f7d0eb
JoselitoNascimento <joselito_nascimento@yahoo.com.br>
2021-08-26 18:36:35 -0300

    Primeiro commit

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home-gradient">
+    <h1 class="home-title">Manuel Cavalcante Advogados Associados</h1>  
+    <div class="main-content__body">
+      <img class="home-image" src="../static/Tela-Abertura.jpg" width="850" height="450">
+    </div>
+  </div>
+</template>
+
+<style>
+
+.home-image {
+    position: relative;
+    width:100%;
+    height:100%;
+    left: 0%;
+    margin-top: -7%;
+    opacity: 0.5;
+}
+.home-title {
+    position: relative;
+    margin-top: 2%;
+    margin-left: 0%;    
+    text-align: center;
+    font-size: 52px;
+    font-weight: bold;
+    font-family: Roboto, Arial, Helvetica, sans-serif;
+    color:  rgba(23, 2, 139, 0.918);
+}
+.home-gradient {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
+  background-color: rgb(243, 235, 235); /* For browsers that do not support gradients */
+  background-image: linear-gradient(rgb(217, 232, 241), rgb(255, 255, 255));
+}
+
+</style>
